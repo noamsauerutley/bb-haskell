7dc89fdfb171e826fa5b820a1d446cfb173707d5
Noam Sauer-Utley <noamsauerutley@gmail.com>
2021-05-09 12:25:34 -0300

    Add length

--- Baby.hs
+++ Baby.hs
@@ -1,20 +1,25 @@
 module Baby
-( doubleMe
+( boomBang
+, doubleMe
 , doubleSmallNumber
 , doubleUs
-, boomBang
+, length
 ) where
+
+boomBang :: [Int] -> [[Char]]
+boomBang xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 doubleMe :: Float -> Float
 doubleMe x = x + x
 
-doubleUs :: Float -> Float -> Float
-doubleUs x y = doubleMe x + doubleMe y 
-
 doubleSmallNumber :: Float -> Float
 doubleSmallNumber x  = if x > 100
     then x
-    else x*2
+    else doubleMe x
 
-boomBang :: [Int] -> [[Char]]
-boomBang xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+doubleUs :: Float -> Float -> Float
+doubleUs x y = doubleMe x + doubleMe y 
+
+length' :: [t] -> Int
+length' xs = sum [1 | _ <- xs]
+
